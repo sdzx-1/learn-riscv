9eeaf7c451e97d4f66677d1b327a16813a51f28d
sdzx-1 <shangdizhixia1993@163.com>
2024-12-11 23:56:02 +0800

    Fix build.zig

--- build.zig
+++ build.zig
@@ -1,33 +1,27 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{
+        .cpu_arch = .riscv32,
+        .os_tag = .freestanding,
+        .abi = .gnuilp32,
+    } });
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
+    const exe = b.addExecutable(.{
         .name = "kernel",
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    exe.addAssemblyFile(b.path("src/asm/start.S"));
 
-    const csept = b.addAssembly(.{
-        .name = "start",
-        .source_file = b.path("src/asm/start.S"),
-        .optimize = optimize,
-        .target = target,
-    });
-    csept.linkLibrary(lib);
-    // b.installArtifact(csept);
-
-    // lib.step.dependOn(&csept.step);
-    // csept.linkLibrary(lib);
-    b.installArtifact(lib);
+    b.installArtifact(exe);
 
     //
     const waf = b.addWriteFiles();
-    waf.addCopyFileToSource(lib.getEmittedAsm(), "root.asm");
-    waf.step.dependOn(&lib.step);
+    waf.addCopyFileToSource(exe.getEmittedAsm(), "root.asm");
+    waf.step.dependOn(&exe.step);
     b.getInstallStep().dependOn(&waf.step);
 }
