73191e8b95ead543b783fac2ae5a555cb2ef348a
sdzx-1 <shangdizhixia1993@163.com>
2024-12-16 19:46:26 +0800

    Add

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,11 @@
 comptime {
     asm (
-        \\.macro do_nothing
-        \\       nop
-        \\       nop
-        \\.endm
-        \\
         \\       .text
         \\       .global _start
         \\_start:
-        \\       li x6, 5
-        \\       li x7, 4 
+        \\       li x6, 1
+        \\       li x7, 2 
         \\       add x5, x6, x7
-        \\       do_nothing
         \\       j main
     );
 }
@@ -19,3 +13,14 @@ comptime {
 export fn main() void {
     while (true) {}
 }
+
+// opcode 7
+// OP  [6:5] [4:2] [1:0]
+// ADD 01    100   11
+
+// R-type Register 3   rs2 sr2 rd
+// I-type Immediate
+// S-type Store
+// B-type Branch
+// U-type Upper
+// J-type Jump
