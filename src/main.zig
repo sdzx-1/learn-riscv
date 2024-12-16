82059358ceb08534ba7e4df2060cc39d0cacc105
sdzx-1 <shangdizhixia1993@163.com>
2024-12-16 23:43:51 +0800

    cc

--- src/main.zig
+++ src/main.zig
@@ -15,6 +15,20 @@ comptime {
     );
 }
 
+var va: i32 = 1;
+var vb: i32 = 2;
+
 export fn main() void {
+    va = sum(va, vb);
     while (true) {}
+}
+
+export fn sum(a: i32, b: i32) i32 {
+    var c: i32 = 0;
+    asm volatile ("add %[sum], %[add1], %[add2]"
+        : [sum] "=r" (c),
+        : [add1] "r" (a),
+          [add2] "r" (b),
+    );
+    return c;
 }
