974a22e7111829b331691e134db0bacb21cb3421
sdzx-1 <shangdizhixia1993@163.com>
2024-12-16 20:26:19 +0800

    cc

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ comptime {
         \\       .global _start
         \\_start:
         \\       li x6, 1
-        \\       li x7, 2 
+        \\       li x7, -2 
         \\       add x5, x6, x7
         \\
         \\loop:
